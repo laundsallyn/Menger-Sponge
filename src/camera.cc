8503e844016745d9f32eff6740038172ed21ba7f
laundsallyn <yipu.wang@utexas.edu>
2017-03-02 16:09:42 -0600

    left drag and right drag work correctly now

--- src/camera.cc
+++ src/camera.cc
@@ -1,6 +1,6 @@
 #include "camera.h"
 #include "glm/gtc/matrix_transform.hpp"
-
+using namespace std;
 glm::mat4 computeLookAt(glm::vec3 eye, glm::vec3 center, glm::vec3 up){
     glm::vec3 const f(glm::normalize(center - eye));
     glm::vec3 const s(glm::normalize(cross(f, up)));
@@ -19,81 +19,89 @@ glm::mat4 computeLookAt(glm::vec3 eye, glm::vec3 center, glm::vec3 up){
     Result[3][0] =-glm::dot(s, eye);
     Result[3][1] =-glm::dot(u, eye);
     Result[3][2] = glm::dot(f, eye);
-   return Result;
+    return Result;
 }
 
-void Camera::computeMatricesFromInputs(GLFWwindow *window){
+void Camera::computeMatricesFromInputs(GLFWwindow *window, double &init_x, double &init_y){
+
     // glfwGetTime is called only once, the first time this function is called
-        static double lastTime = glfwGetTime();
+    static double lastTime = glfwGetTime();
+    // Compute time difference between current and last frame
+    double currentTime = glfwGetTime();
+    deltaTime = float(currentTime - lastTime);
 
-        // Compute time difference between current and last frame
-        double currentTime = glfwGetTime();
-        float deltaTime = float(currentTime - lastTime);
+    // Get mouse position
+    double xpos, ypos;
+    glfwGetCursorPos(window, &xpos, &ypos);
 
-        // Get mouse position
-        double xpos, ypos;
-        glfwGetCursorPos(window, &xpos, &ypos);
 
-        // Reset mouse position for next frame
-//        glfwSetCursorPos(window, 1920 / 2, 1280 / 2);
+    if(glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS){
+        // Compute new orientation
+        float deltaX = (float)(init_x - xpos);
+        float deltaY = (float)(init_y - ypos);
 
-        if(glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS){
-            // Compute new orientation
-            horizontalAngle += mouseSpeed * float(1920 / 2 - xpos)/30;
-            verticalAngle += mouseSpeed * float(1280 / 2 - ypos)/30;
-            //restrict vertical angle
-            //verticalAngle = 0;
+        horizontalAngle += mouseSpeed * deltaX /30;
+        verticalAngle += mouseSpeed * deltaY/30;
 
+    }
+    if(glfwGetMouseButton(window,GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS){
+        if (init_y - ypos < 0.0){
+            camera_distance_ -= deltaTime * speed;
+            position.z += deltaTime * speed;
+        }else if (init_y - ypos > 0.0){
+            camera_distance_ += deltaTime * speed;
+            position.z -= deltaTime * speed;
         }
 
+    }
 
 
-        // Direction : Spherical coordinates to Cartesian coordinates conversion
-        glm::vec3 direction(
-            cos(verticalAngle) * sin(horizontalAngle),
-            sin(verticalAngle),
-            cos(verticalAngle) * cos(horizontalAngle)
-            );
+    // Direction : Spherical coordinates to Cartesian coordinates conversion
+    glm::vec3 direction (
+                cos(verticalAngle) * sin(horizontalAngle),
+                sin(verticalAngle),
+                cos(verticalAngle) * cos(horizontalAngle)
+                );
 
-        // Right vector
-        glm::vec3 right = glm::vec3(
-            sin(horizontalAngle - 3.14f / 2.0f),
-            0,
-            cos(horizontalAngle - 3.14f / 2.0f)
-            );
+    // Right vector
+    glm::vec3 right = glm::vec3(
+                sin(horizontalAngle - 3.14f / 2.0f),
+                0,
+                cos(horizontalAngle - 3.14f / 2.0f)
+                );
 
-        // Up vector
-        glm::vec3 up = glm::cross(right, direction);
+    // Up vector
+    glm::vec3 up = glm::cross(right, direction);
 
-        // Move forward
-        if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
-            position += direction * deltaTime * speed;
-        }
-        // Move backward
-        if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
-            position -= direction * deltaTime * speed;
-        }
-        // Strafe right
-        if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
-            position += right * deltaTime * speed;
-        }
-        // Strafe left
-        if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
-            position -= right * deltaTime * speed;
-        }
+    // Move forward
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+        position += direction * deltaTime * speed;
+    }
+    // Move backward
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+        position -= direction * deltaTime * speed;
+    }
+    // Strafe right
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+        position += right * deltaTime * speed;
+    }
+    // Strafe left
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+        position -= right * deltaTime * speed;
+    }
 
-        float FoV = initialFoV;// - 5 * glfwGetMouseWheel(); // Now GLFW 3 requires setting up a callback for this. It's a bit too complicated for this beginner's tutorial, so it's disabled instead.
+    float FoV = initialFoV;// - 5 * glfwGetMouseWheel(); // Now GLFW 3 requires setting up a callback for this. It's a bit too complicated for this beginner's tutorial, so it's disabled instead.
 
-                               // Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
-        ProjectionMatrix = glm::perspective(FoV, 4.0f / 3.0f, 0.1f, 100.0f);
-        // Camera matrix
-        ViewMatrix = computeLookAt(
-            position,           // Camera is here
-            position + direction, // and looks here : at the same position, plus "direction"
-            up                  // Head is up (set to 0,-1,0 to look upside-down)
-            );
+    // Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+    ProjectionMatrix = glm::perspective(FoV, 4.0f / 3.0f, 0.1f, 100.0f);
+    // Camera matrix
+    ViewMatrix = computeLookAt(
+                position,           // Camera is here
+                position + direction, // and looks here : at the same position, plus "direction"
+                up                  // Head is up (set to 0,-1,0 to look upside-down)
+                );
 
-        // For the next frame, the "last time" will be "now"
-        lastTime = currentTime;
+    // For the next frame, the "last time" will be "now"
+    lastTime = currentTime;
 }
 
--- src/camera.h
+++ src/camera.h
@@ -4,9 +4,26 @@
 #include <glm/glm.hpp>
 #include "glm/ext.hpp"
 #include <GLFW/glfw3.h>
-
+#include <iostream>
 
 class Camera {
+private:
+    glm::mat4 ViewMatrix;
+    glm::mat4 ProjectionMatrix;
+    // Initial position : on +Z
+    float camera_distance_ = 3.0;
+    float deltaTime;
+    glm::vec3 position ;
+    // Initial horizontal angle : toward -Z
+    float horizontalAngle = 3.14f;
+    // Initial vertical angle : none
+    float verticalAngle = 0.0f;
+    // Initial Field of View
+    float initialFoV = 45.0f;
+
+
+    float speed = 13.0f; // 3 units / second
+    float mouseSpeed = 0.002f;
 public:
 
 	glm::mat4 get_view_matrix() const{
@@ -15,32 +32,8 @@ public:
 	glm::mat4 get_projection_matrix () const{
         return ProjectionMatrix;
     }
-    void computeMatricesFromInputs(GLFWwindow *);
-    void zoomIn(){camera_distance_ -= zoomSpeed;}
-    void zoomOut(){camera_distance_ += zoomSpeed;}
+    void computeMatricesFromInputs(GLFWwindow *, double&, double&);
 
-	// FIXME: add functions to manipulate camera objects.
-private:
-	glm::mat4 ViewMatrix;
-	glm::mat4 ProjectionMatrix;
-    // Initial position : on +Z
-    float camera_distance_ = 3.0;
-
-    glm::vec3 position = glm::vec3(0, 0, camera_distance_);
-    // Initial horizontal angle : toward -Z
-    float horizontalAngle = 3.14f;
-    // Initial vertical angle : none
-    float verticalAngle = 0.0f;
-    // Initial Field of View
-    float initialFoV = 45.0f;
-
-    float speed = 3.0f; // 3 units / second
-    float mouseSpeed = 0.002f;
-    float zoomSpeed = 10.2f;
-//	glm::vec3 look_ = glm::vec3(0.0f, 0.0f, -1.0f);
-//	glm::vec3 up_ = glm::vec3(0.0f, 1.0, 0.0f);
-//	glm::vec3 eye_ = glm::vec3(0.0f, 0.0f, camera_distance_);
-//	// Note: you may need additional member variables
 
 };
 
